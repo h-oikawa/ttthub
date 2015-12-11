5e2fd90b09769496e15cc993b6ab5bc400d55a88
hiroaki <ar2ngement@hotmail.com>
2015-12-12 08:56:58 +0900

    1つめ

--- app/src/main/java/ho/ttthub/MainActivity.java
+++ app/src/main/java/ho/ttthub/MainActivity.java
@@ -22,6 +22,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     /**
+     * github from android studio
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
      * {@link FragmentPagerAdapter} derivative, which will keep every
